cd68c68447bafe4a4248e84db27815eaadf26887
Codaea <dakotajayroth@gmail.com>
2025-08-27 00:15:57 -0700

    Refactor: Load environment variables using Overload and add systemd service file for thermal printer

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"image"
 	"image/color"
 	"image/png"
-	"log"
 	"os"
 	"strings"
 
@@ -19,10 +18,7 @@ import (
 
 func main() {
 
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	_ = godotenv.Overload(".env")
 
 	// printer setup
 	printerPath, found := os.LookupEnv("PRINTER_PATH")
