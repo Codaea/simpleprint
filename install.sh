b92bfbc18469340db03bb015806ad8e82bf1b15f
Codaea <dakotajayroth@gmail.com>
2025-09-11 21:53:38 -0700

    Update working directory path in installation script to use /tmp/simpleprint

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ REPO="codaea/simpleprint"
 SERVICE_NAME="simpleprint"
 INSTALL_DIR="/usr/bin"
 SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
-WORK_DIR="/home/coda/simpleprint"
+WORK_DIR="/tmp/simpleprint"
 
 # Detect architecture
 ARCH=$(uname -m)
